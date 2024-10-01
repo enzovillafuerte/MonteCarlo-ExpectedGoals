d5354a6d15da5588c26e918422982901856d97a1
Enzo Villafuerte <ev693020@ohio.edu>
2024-09-30 22:31:47 -0400

    Add files via upload

---
+++ MonteCarlo_Project.R
@@ -0,0 +1,5 @@
+# MonteCarlo Project
+# Define Scope and Objective
+# Ideas
+# Use of xG to predict the outcome of a game based on xG instead of Poisson
+# https://github.com/sonofacorner/matplotlib-tutorials/blob/main/intro_monte_carlo/main_notebook.ipynb
