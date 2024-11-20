96ff7e9304f52822bde2d164912c5b91538b112a
Enzo Villafuerte <ev693020@ohio.edu>
2024-11-19 23:06:51 -0500

    CSV in Repo

--- MonteCarlo_Project.R
+++ MonteCarlo_Project.R
@@ -1,5 +1,38 @@
 # MonteCarlo Project
 # Define Scope and Objective
-# Ideas
 # Use of xG to predict the outcome of a game based on xG instead of Poisson
 # https://github.com/sonofacorner/matplotlib-tutorials/blob/main/intro_monte_carlo/main_notebook.ipynb
+# setwd('Documents/Github/MonteCarlo-ExpectedGoals')
+
+
+# Import libraries
+library(readr)
+
+
+
+## 1. API Connection Section
+
+
+
+## 2. Function Generating based on a sample (Barca vs Man Utd 2011.csv)
+
+# Read the dataset
+data <- read.csv('BarcavsManUtd2011.csv')
+
+head(data)
+colnames(data)
+
+
+# Filter dataframe
+# 1st Filter -> Keep Only shots data
+# Where shot_statsbomb_xg IS NOT NULL
+
+
+
+
+
+head(data)
+length(data)
+# Function simulating a match
+
+# kee
