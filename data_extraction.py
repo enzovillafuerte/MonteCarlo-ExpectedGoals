9ec3575e20f8334b52d220f16275b73e47da632a
Enzo Villafuerte <ev693020@ohio.edu>
2024-11-20 22:41:23 -0500

    data_extraction.py

---
+++ data_extraction.py
@@ -0,0 +1,54 @@
+import pandas as pd 
+import numpy as np 
+from statsbombpy import sb
+
+# Potential Title of the Study
+# Did Peru actually deserve to advance to Round of 16 based on xG?
+
+# Statsbombpy API - Github link: https://github.com/statsbomb/statsbombpy
+# Fortunately, the API follows similar syntax for manipulation as pandas
+
+# See competitions available in statsboms free version
+# print(sb.competitions()) # - Interested in WorldCup from 2018. Want to see who deserved to win and subsequently to advance ahead to the next round.
+
+# Competition_id = 3 -> WorldCup, season_id=3 -> 2018
+a = sb.matches(competition_id=43, season_id=3)
+
+# Only keeping data from the group stage
+a = a[a['competition_stage'] == 'Group Stage']
+
+# Keeping records of teams that were part of the group
+# [7530, 7532, 7546, 7547, 7562, 7563]
+list_of_interest = ['Peru', 'France', 'Denmark', 'Australia']
+a = a[a['home_team'].isin(list_of_interest)]
+
+# Order it in ascending order to reflect the order of the games in reality
+a = a.sort_values(by='match_id', ascending=True)
+
+# Extracting match_id for each game and storing them into a list
+matches_list = a['match_id'].to_list()
+
+# Build a function to cleanse data programatically
+def data_cleansing(match_id):
+
+    df = sb.events(match_id = match_id)
+
+    # Only keeping records where 'shot_statsbomb_xg' is not null
+    df = df[df['shot_statsbomb_xg'].notnull()]
+    
+    # Adding the identifier for the game
+    df['match_id'] = match_id
+
+    return df
+
+
+print(data_cleansing(7532).columns)
+
+# To-do Next: Automate extraction for other matches using dictionary and concatenate all into a consolidated dataset
+# Only then start coding in R
+#       - Probabilities of each team winning
+#       - xG Flowchart + Probability visualization
+#       - 3 points for the team that deserved to win
+#       - Expected Points Model to see if we should have passed
+# Peru vs Denmark: 7532
+# To run: python data_extraction.py
