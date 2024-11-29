d880ca09f6a0c31fe634b6338b719999a4df33e3
Enzo Villafuerte <ev693020@ohio.edu>
2024-11-28 20:27:33 -0500

    Update MonteCarlo_Project.R

--- MonteCarlo_Project.R
+++ MonteCarlo_Project.R
@@ -1,18 +1,7 @@
 # MonteCarlo Project
-# Define Scope and Objective
-# Use of xG to predict the outcome of a game based on xG instead of Poisson
-# https://github.com/sonofacorner/matplotlib-tutorials/blob/main/intro_monte_carlo/main_notebook.ipynb
+
 # setwd('Documents/Github/MonteCarlo-ExpectedGoals')
-
-# To-do Next: Automate extraction for other matches using dictionary and concatenate all into a consolidated dataset
-
-# Only then start coding in R
-#       - Probabilities of each team winning
-#       - xG Flowchart + Probability visualization
-#       - 3 points for the team that deserved to win
-#       - Expected Points Model to see if we should have passed
 # Peru vs Denmark: 7532
-
 
 # Import libraries
 library(readr)
